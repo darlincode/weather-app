865e756c129941d2ce43788cd3f25c5c2990ed84
darlincode <nwannachukwuemekad@gmsil.com>
2022-02-02 17:05:08 +0100

    update

--- lib/widgets/WeatherItem.dart
+++ lib/widgets/WeatherItem.dart
@@ -13,7 +13,7 @@ class WeatherItem extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text('New York', style: new TextStyle(color: Colors.black)),
-            Text('Rain', style: new TextStyle(color: Colors.black, fontSize: 24.0)),
+            Text('Rain', style: new TextStyle(color: Colors.black, fontSize: 25.1)),
             // ignore: prefer_const_constructors
             Text('72°F',  style: new TextStyle(color: Colors.black)),
             Image.network('https://openweathermap.org/img/w/01d.png'),
